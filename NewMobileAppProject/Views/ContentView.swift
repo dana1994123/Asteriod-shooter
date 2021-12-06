6187c718050c4e4cc95bfc68332dbd5104bc78ab
dana <danaaljamal94@gmail.com>
2021-12-06 11:32:16 -0500

    Home Page navigation

--- NewMobileAppProject/Views/ContentView.swift
+++ NewMobileAppProject/Views/ContentView.swift
@@ -12,37 +12,38 @@ struct ContentView: View {
     @EnvironmentObject var fireDBHelper : FireDBHelper
     var body: some View {
         VStack{
-            //all the modifier are under modifier Group
-            Text("I'm title 1").modifier(Header1())
-            Text("I'm title 2").modifier(Header2())
-            Text("I'm title 3").modifier(Header3())
-            Text("Error").modifier(Error())
-            VStack{
-                TextField( "Enter your Name" , text: self.$textEdit).modifier(TextFieldModifier())
-                Button(action:{
-                    print("click me")
-                    self.addScore()
-                }){
-                    Text("Click").modifier(MainAppButtonModifier())
-                }
-                Button(action:{
-                    print("click me")
-                }){
-                    Text("Click 2").modifier(AppButtonModifier())
-                }
-                List{
-                    ForEach(self.fireDBHelper.scoreLit.enumerated().map({$0}), id : \.element.self){index, currentScore in
-                        Text(currentScore.name)
-                        Text(String(currentScore.score))
-                        
-                    }
-                }
-                
-                
+            HomeUIView()
+//            //all the modifier are under modifier Group
+//            Text("I'm title 1").modifier(Header1())
+//            Text("I'm title 2").modifier(Header2())
+//            Text("I'm title 3").modifier(Header3())
+//            Text("Error").modifier(Error())
+//            VStack{
+//                TextField( "Enter your Name" , text: self.$textEdit).modifier(TextFieldModifier())
+//                Button(action:{
+//                    print("click me")
+//                    self.addScore()
+//                }){
+//                    Text("Click").modifier(MainAppButtonModifier())
+//                }
+//                Button(action:{
+//                    print("click me")
+//                }){
+//                    Text("Click 2").modifier(AppButtonModifier())
+//                }
+//                List{
+//                    ForEach(self.fireDBHelper.scoreLit.enumerated().map({$0}), id : \.element.self){index, currentScore in
+//                        Text(currentScore.name)
+//                        Text(String(currentScore.score))
+//
+//                    }
+//                }
+//
+//
             }//vstack
-        }.onAppear(){
-            self.fireDBHelper.getAllScore()
-        }//hstack
+//        }.onAppear(){
+//            self.fireDBHelper.getAllScore()
+//        }//hstack
     }
     private func addScore (){
         print("adding scores to the firebase")
--- NewMobileAppProject/Views/HomeUIView.swift
+++ NewMobileAppProject/Views/HomeUIView.swift
@@ -10,16 +10,36 @@ import SwiftUI
 struct HomeUIView: View {
     var body: some View {
         NavigationView{
-            NavigationLink(destination: ScoreBoard() ){
-                Text("score Board")
-            }
-            NavigationLink(destination: ScoreBoard() ){
-                Text("Start the Game")
-            }
+            ZStack{
+                Image("bg").resizable().frame(width: 980, height: 460, alignment: .center)
+                VStack{
+                    Text("GET READY FOR").modifier(Header2())
+                    ZStack{
+                        Image("game").resizable().frame(width: 500, height:570, alignment: .center)
+                        Image("logo").resizable().frame(width: 900, height:300, alignment: .center)
+                    }.frame(width: 1000, height: 260, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    HStack{
+                        NavigationLink(destination: ScoreBoard() ){
+                            Text("score Board").modifier(AppButtonModifier())
+                        }
+                        NavigationLink(destination: ScoreBoard() ){
+                            Text("Start the Game").modifier(AppButtonModifier())
+                        }
+                        NavigationLink(destination: ScoreBoard() ){
+                            Text("Start the Game").modifier(AppButtonModifier())
+                        }
+                    }//vstack2
             
-        }
+                }//vstack
+            }//zstack
+            .navigationBarHidden(true)
+            
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
-}
+                              }
+            
+                              
 
 struct HomeUIView_Previews: PreviewProvider {
     static var previews: some View {
