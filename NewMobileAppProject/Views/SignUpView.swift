faf93c2103ceb5d8d429cf3cc3733ddf21a666c7
Roop Malhans <malhanro@sheridancollege.ca>
2021-12-07 20:48:09 -0500

    Sign Up View Created

--- NewMobileAppProject/Views/SignUpView.swift
+++ NewMobileAppProject/Views/SignUpView.swift
@@ -9,10 +9,52 @@ import SwiftUI
 
 struct SignUpView: View {
     @State private var username: String = ""
+    @State private var email: String = ""
     @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+
     let verticalPadding = 40.0
     var body: some View {
-       Text("thgh")
+        ZStack
+               {
+               VStack(spacing: CGFloat(verticalPadding)) {
+                   Text("Asteroid Shooter").modifier(Header1())
+
+                   HStack {
+                       Image(systemName: "person")
+                           .foregroundColor(.secondary)
+                       TextField("Enter your name", text: $username)
+
+                   }.modifier(TextFieldModifier())
+                   HStack {
+                       Image(systemName: "folder")
+                           .foregroundColor(.secondary)
+                       TextField("Enter your Email", text: $email)
+
+                   }.modifier(TextFieldModifier())
+                   
+                   HStack {
+                       Image(systemName: "key")
+                           .resizable()
+                           .frame(width: 16.0, height: 20.0) .foregroundColor(.secondary)
+                       SecureField("Enter password", text: $password)
+                   }.modifier(TextFieldModifier())
+                   HStack {
+                       Image(systemName: "key")
+                           .resizable()
+                           .frame(width: 16.0, height: 20.0) .foregroundColor(.secondary)
+                       SecureField("Confirm password", text: $confirmPassword)
+                   }.modifier(TextFieldModifier())
+                   
+                   HStack{
+                       Text("Already have an account? ")
+                       NavigationLink(destination: LoginView()){
+                           Text("Login")
+                   }
+
+               }
+               }
+               }
     }
 }
 
