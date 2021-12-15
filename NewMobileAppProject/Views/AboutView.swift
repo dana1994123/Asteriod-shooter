5b9adcab69296c58dc81cce253816f4316b9741e
Jashanpreet Singh <jshnpreet.singh.mann@gmail.com>
2021-12-15 15:39:55 -0500

    Roop: Image added in AboutUS

--- NewMobileAppProject/Views/AboutView.swift
+++ NewMobileAppProject/Views/AboutView.swift
@@ -10,21 +10,21 @@ import SwiftUI
 struct AboutView: View {
     var person : People
     var body: some View {
-            HStack {
+        HStack {
                 VStack{
                     Text("Name: \(person.Name)")
                         .font(.headline)
                         .foregroundColor(.black)
                         .multilineTextAlignment(.leading)
-                        .frame(width: 300, height: 30, alignment: .leading)
+                        .frame(width: 300, height: 25, alignment: .center)
                     Text("Bio: \(person.Bio)")
                         .font(.subheadline)
                         .foregroundColor(.yellow)
-                     .frame(width: 400, height: 30, alignment: .leading)
+                     .frame(width: 300, height: 25, alignment: .center)
                     
                 }
-               
-            }.frame(width: 30, height: 60, alignment: .trailing)//hstack
+                    
+                }.frame(width: 300, height: 60, alignment: .center)//hstack
     }
 }
 
@@ -34,7 +34,8 @@ struct ListView: View {
         ZStack{
             Image("bg2").resizable().frame(width: 1000, height: 1000, alignment: .center).opacity(0.8)
             VStack{
-                Text("About Us").font(.title).modifier(Header1())
+                Image("aboutus")
+
                 ForEach(fetch.people, id: \.self){ p in
                     AboutView(person: p)
                 }
