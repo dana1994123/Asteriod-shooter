7ff85685621b63cd056d6d2277d51a15df25a745
Jashanpreet Singh <jshnpreet.singh.mann@gmail.com>
2021-12-15 00:12:51 -0500

    Roop's Sign Up View Completed (Her Github was not working so I am commiting from my end)

--- NewMobileAppProject/Views/SignUpView.swift
+++ NewMobileAppProject/Views/SignUpView.swift
@@ -6,30 +6,41 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct SignUpView: View {
+
     @State private var username: String = ""
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var confirmPassword: String = ""
+    @State private var error : String = ""
+    @State private var checkingUser = false
+    @State private var isShowingHomeView = false
 
     let verticalPadding = 40.0
     var body: some View {
+        NavigationView{
         ZStack
                {
+                   ScrollView{
                VStack(spacing: CGFloat(verticalPadding)) {
                    Text("Asteroid Shooter").modifier(Header1())
+                       
 
                    HStack {
                        Image(systemName: "person")
                            .foregroundColor(.secondary)
                        TextField("Enter your name", text: $username)
+                           .autocapitalization(.none)
+                       
 
                    }.modifier(TextFieldModifier())
                    HStack {
                        Image(systemName: "folder")
                            .foregroundColor(.secondary)
                        TextField("Enter your Email", text: $email)
+                           .autocapitalization(.none)
 
                    }.modifier(TextFieldModifier())
                    
@@ -38,24 +49,70 @@ struct SignUpView: View {
                            .resizable()
                            .frame(width: 16.0, height: 20.0) .foregroundColor(.secondary)
                        SecureField("Enter password", text: $password)
+                           .autocapitalization(.none)
                    }.modifier(TextFieldModifier())
                    HStack {
                        Image(systemName: "key")
                            .resizable()
                            .frame(width: 16.0, height: 20.0) .foregroundColor(.secondary)
                        SecureField("Confirm password", text: $confirmPassword)
+                           .autocapitalization(.none)
                    }.modifier(TextFieldModifier())
-                   
+                   Text("\(self.error)").modifier(Error())
                    HStack{
                        Text("Already have an account? ")
                        NavigationLink(destination: LoginView()){
                            Text("Login")
                    }
+                       
 
                }
+                   NavigationLink(destination: HomeUIView(), isActive: $isShowingHomeView) { EmptyView() }
+                   Button(action:{
+                       if(self.checking()){
+                      // AddInfo(UserName: username, Email: email, Password: password, ConfirmPassword: confirmPassword)
+                    self.isShowingHomeView = self.AddInfo(UserName: username, Email: email, Password: password, ConfirmPassword: confirmPassword)
+
+                       }
+                   })
+                   {
+                       Text("Register")
+                   }.modifier(AppButtonModifier()).padding(.bottom , 90)
+                
+                   
+               }.onAppear(perform: {
+                   self.checkingUser = false
+               })
                }
                }
+        }.navigationViewStyle(StackNavigationViewStyle())
+        
     }
+    
+    func AddInfo(UserName: String, Email: String, Password: String, ConfirmPassword: String) -> Bool{
+        let db = Firestore.firestore()
+        db.collection("Users").document().setData(["UserName": UserName, "Email": Email, "Password": Password, "ConfirmPassword": ConfirmPassword])
+        return true
+    }
+    
+    func checking() -> Bool{
+        if(self.username.isEmpty || self.email.isEmpty || self.password.isEmpty || self.confirmPassword.isEmpty){
+            self.error = "Please enter the required information"
+            return false
+        }
+        else if (self.password != self.confirmPassword)
+        {
+            self.error = "Both Passwords should match"
+            return false
+        }
+        else{
+            self.checkingUser = true
+            self.error = ""
+            return true
+        }
+    }
+    
+    
 }
 
 
